e1634bf7cf226733c31ecae20283fb1266254d1d
zaradiputranto <nazarafaliya@gmail.com>
2025-05-23 07:50:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("c34cbcdcf6f065e8c2452199042f63fb.jpg", width=200)
