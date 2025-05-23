a47af19135ac31a8f168a664bb7507380f3ae8f9
zaradiputranto <nazarafaliya@gmail.com>
2025-05-23 07:54:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("zara")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "hello."
 )
 st.image("c34cbcdcf6f065e8c2452199042f63fb.jpg", width=200)
